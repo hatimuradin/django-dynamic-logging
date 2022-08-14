fb5b1c0dcc6a1d800b431fc05f62edb0a801c7c8
hatimuradin <hatefmdn@gmail.com>
2022-08-14 11:24:55 +0430

    Rename project directories

--- src/dynamic-logging/asgi.py
+++ src/dynamic-logging/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for splunktest project.
+ASGI config for dynamic-logging project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "splunktest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dynamic-logging.settings")
 
 application = get_asgi_application()
--- src/dynamic-logging/wsgi.py
+++ src/dynamic-logging/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for splunktest project.
+WSGI config for dynamic-logging project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "splunktest.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dynamic-logging.settings")
 
 application = get_wsgi_application()
